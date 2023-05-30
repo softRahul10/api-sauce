39d8fa8771b4eecac38c179aa064814ca220d991
softRahul10 <softrahul10@gmail.com>
2023-05-30 12:41:32 +0530

    API is Ready

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const  app = express();
 
-//app.get();
+app.get('/random/:min/:max', (req,res) => {
+    let minNumber = parseInt(req.params.min);
+    let maxNumber = parseInt(req.params.max);
+    
+    if(isNaN(minNumber) || isNaN(maxNumber)) {
+        res.status(400);
+        res.json({ error: 'Bad Request' });
+        return;
+    }
+
+    var result = Math.round((Math.random() * (maxNumber - minNumber)) + minNumber); 
+    res.json({ result: result }); 
+
+});
 
 app.listen(3000,() =>  console.log('Server listening on port number 3000'));
